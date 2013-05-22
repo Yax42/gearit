55e7e532c553f1128fec3f593eec1b3d87ee5a98
Jean Brunier <brunier42@gmail.com>
2013-05-22 12:14:54 +0200

    Add basics and farseer

---
+++ src/Heart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FarseerPhysics.Collision.Shapes;
+using FarseerPhysics.Common;
+using Microsoft.Xna.Framework;
+
+namespace gearit
+{
+  class Heart
+  {
+    private PolygonShape	_polygon;
+    private Vertices		_vertices; //Le PolygonShape sera composé de ces vertices.
+
+    public Heart()
+    {
+      Console.WriteLine("new heart.");
+      _vertices = new Vertices(50);
+      _vertices.Add(new Vector2(0, 30));
+      _vertices.Add(new Vector2(30, 30));
+      _vertices.Add(new Vector2(30, 0));
+      _polygon = new PolygonShape(_vertices, 0);
+    }
+  }
+}
---
+++ src/main.cs
@@ -0,0 +1,16 @@
+﻿using FarseerPhysics.Dynamics;
+using Microsoft.Xna.Framework;
+
+namespace gearit
+{
+  class main
+  {
+
+    public static void Main()
+    {
+        World world = new World(new Vector2(0, 9.82f));
+
+        Robot WallE = new Robot();
+    }
+  }
+}
