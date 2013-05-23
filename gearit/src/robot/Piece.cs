90d8c48f0914bf39ccca419574c3667c09a923d3
Jean Brunier <brunier42@gmail.com>
2013-05-23 11:15:47 +0200

    Clean again.

---
+++ gearit/src/robot/Piece.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Collision.Shapes;
+
+namespace gearit
+{
+    class Piece : Body
+    {
+	Shape	    _shape;
+	Fixture	    _fix;
+
+	internal Piece(World world, Shape shape) :
+		base(world)
+	 {
+            _shape = shape;
+            _fix = CreateFixture(_shape, null);
+	 }
+    }
+}
