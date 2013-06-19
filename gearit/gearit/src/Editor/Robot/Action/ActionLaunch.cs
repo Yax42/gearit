e940d9888b012d9c30028088b217d7810184d071
Raphael Monnerat <rmonnerat.pro@gmail.com>
2013-06-19 15:22:39 +0200

    Skeleton for ActionLaunch.

---
+++ gearit/gearit/src/Editor/Robot/Action/ActionLaunch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using gearit.src.utility;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace gearit.src.editor.robot.action
+{
+    class ActionLaunch : IAction
+    {
+        public void init() { }
+
+        public ActionTypes run(Input input, Robot robot, ref Piece selected)
+        {
+            robot.getWorld().Gravity = new Vector2(0f, 9.8f);
+            return (ActionTypes.LAUNCH);
+        }
+    }
+}
