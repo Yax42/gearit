9784117554af9d54ae40c4f2b1224d9d112d4db4
Aliou Diallo <aliou.diallo@epitech.net>
2013-06-22 10:44:50 +0200

    Add an helper class for the Serialization of GI objects.
    * `SerializerHelper` class will store the objects used for deserializing
    the GearIt objects.

---
+++ gearit/gearit/src/Editor/SerializerHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FarseerPhysics.Dynamics;
+
+namespace gearit.src.editor
+{
+    /// <summary>
+    /// Helper class for the serialization of Gear It objects.
+    /// </summary>
+    static class SerializerHelper
+    {
+	public static World _world = null;
+    }
+}
