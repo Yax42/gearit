d107d0ccf5c0a23850a7fd8549d3ee2916207cb4
Aliou Diallo <aliou.diallo@epitech.net>
2013-06-24 15:08:00 +0200

    Serialize the fixtures contained in the Bodies.
    * Serialize a Fixture by storing the necessary values in a structure.
    * Recreate the fixtures with a default PolygonShape.

--- gearit/gearit/src/Editor/SerializerHelper.cs
+++ gearit/gearit/src/Editor/SerializerHelper.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using FarseerPhysics.Dynamics;
+using FarseerPhysics.Collision.Shapes;
+using Microsoft.Xna.Framework;
 
 namespace gearit.src.editor
 {
@@ -25,11 +27,50 @@ namespace gearit.src.editor
     }
 
     /// <summary>
+    /// Structure holding the necessary information to recreate a Fixture.
+    /// </summary>
+    public struct SerializedFixture
+    {
+        private float _density;
+        private float _friction;
+
+        /// <summary>
+        /// Converts a fixture to a SerializedFixture.
+        /// </summary>
+        /// <param name="f">The fixture to convert.</param>
+        /// <returns>The converted fixture.</returns>
+        public static SerializedFixture convertFixture(Fixture f)
+        {
+            SerializedFixture sf = new SerializedFixture();
+
+            sf._density = f.Shape.Density;
+            sf._friction = f.Friction;
+            return (sf);
+        }
+
+        /// <summary>
+        /// Converts a SerializedFixture to a Fixture.
+        /// </summary>
+        /// <param name="sf">The SerializedFixture to convert.</param>
+        /// <param name="b">The necessary body to create the fixture.</param>
+        /// <returns>The converted SerializedFixture.</returns>
+        public static Fixture convertSFixture(SerializedFixture sf, Body b)
+        {
+            Fixture f = new Fixture(b, new PolygonShape(sf._density));
+            f.Friction = sf._friction;
+
+            return (f);
+        }
+    }
+
+    /// <summary>
     /// Structure holding the necessary information to recreate a Body.
     /// </summary>
     public struct SeralizedBody
     {
-        private BodyType _bodytype;
+        private Vector2 _position;
+        private float _rotation;
+        private List<SerializedFixture> _fixtures;
 
         /// <summary>
         /// Converts a Body to a SeralizedBody.
@@ -40,10 +81,13 @@ namespace gearit.src.editor
         {
             SeralizedBody sb = new SeralizedBody();
 
-            sb._bodytype = body.BodyType;
+            sb._position = body.Position;
+            sb._rotation = body.Rotation;
+            sb._fixtures = new List<SerializedFixture>(body.FixtureList.Count());
+            foreach (Fixture f in body.FixtureList)
+                sb._fixtures.Add(SerializedFixture.convertFixture(f));
             return (sb);
         }
-
         /// <summary>
         /// Converts a SeralizedBody to a Body.
         /// </summary>
@@ -53,7 +97,10 @@ namespace gearit.src.editor
         {
             Body b = new Body(SerializerHelper._world);
 
-            b.BodyType = sbody._bodytype;
+            b.Position = sbody._position;
+            b.Rotation = sbody._rotation;
+            foreach (SerializedFixture sf in sbody._fixtures)
+                b.FixtureList.Add(SerializedFixture.convertSFixture(sf, b));
             return (b);
         }
     }
