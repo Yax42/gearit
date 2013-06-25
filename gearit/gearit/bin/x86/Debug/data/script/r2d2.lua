37a99a0b04c82f8b259dc58e1c72974859169f13
Jean Brunier <brunier42@gmail.com>
2013-06-25 17:37:56 +0200

    Update scripts.

--- gearit/gearit/bin/x86/Debug/data/script/r2d2.lua
+++ gearit/gearit/bin/x86/Debug/data/script/r2d2.lua
@@ -1,12 +1,14 @@
 require 'header'
 
 while true do
-	if Input:pressed(K_W) then
+	if Input:pressed(K_A) then
+	  spot0.motor = 1
+	  spot1.motor = 1
+	elseif Input:pressed(K_D) then
 	  spot0.motor = -1
-	  spot2.motor = 1
+	  spot1.motor = -1
 	else
 	  spot0.motor = 0
-	  spot2.motor = 0
+	  spot1.motor = 0
 	end
-
 end
