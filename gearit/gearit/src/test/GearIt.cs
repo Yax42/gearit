04e75211968f81999606376b21fadf37159c199c
Aliou Diallo <code@aliou.me>
2013-11-04 15:37:47 +0100

    Add network skeleton.
    Add network related methods following the Microsoft example:
    http://xbox.create.msdn.com/en-US/education/catalog/sample/network_cs

--- gearit/gearit/src/test/GearIt.cs
+++ gearit/gearit/src/test/GearIt.cs
@@ -1,5 +1,6 @@
 using System;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Net;
 using gearit.xna;
 
 namespace gearit.src.utility
@@ -9,6 +10,15 @@ namespace gearit.src.utility
     /// </summary>
     public class GearIt : Game
     {
+        // TODO: Update values.
+        const int maxGamers = 4;
+        const int maxLocalGamers = 2;
+
+        private NetworkSession networkSession;
+
+        private PacketWriter packetWriter;
+        private PacketReader packetReader;
+
         public GearIt()
         {
             Window.Title = "GearIt - Prototype";
@@ -22,6 +32,9 @@ namespace gearit.src.utility
             FrameRateCounter frameRateCounter = new FrameRateCounter(ScreenManager);
             frameRateCounter.DrawOrder = 101;
             Components.Add(frameRateCounter);
+
+            packetWriter = new PacketWriter();
+            packetReader = new PacketReader();
         }
 
         public ScreenManager ScreenManager { get; set; }
@@ -45,6 +58,48 @@ namespace gearit.src.utility
         {
             MainMenu main = new MainMenu(ScreenManager);
             main.LoadContent();
+        }
+
+        /// <summary>
+        /// Allows the game to run logic.
+        /// </summary>
+        protected override void Update(GameTime gameTime)
+        {
+            // TODO: Input ?
+            if (networkSession == null)
+            {
+                // If we are not in a network session, update the
+                // menu screen that will let us create or join one.
+                UpdateMenuScreen();
+            }
+            else
+            {
+                // If we are in a network session, update it.
+                UpdateNetworkSession();
+            }
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// Menu screen provides options to create or join network sessions.
+        /// </summary>
+        private void UpdateMenuScreen()
+        {
+            // TODO: Sign-in / Create session / Join Session.
+        }
+
+        /// <summary>
+        /// Updates the state of the network session, moving the robots
+        /// around and synchronizing their state over the network.
+        /// </summary>
+        private void UpdateNetworkSession()
+        {
+            // TODO: For each local players (only one?), read input and send
+            // them to the server.
+
+            // TODO: If host of party, update all robots and send positions to
+            // all players.
         }
     }
 }
