2a6c059ce1cc4f828ffb8704f4b0d0bfd40ec08d
Aliou Diallo <code@aliou.me>
2013-11-20 14:43:35 +0100

    Minor indents.

--- gearit/gearit/src/game/GearitGame.cs
+++ gearit/gearit/src/game/GearitGame.cs
@@ -26,7 +26,7 @@ namespace gearit.src.game
         private RectangleOverlay _background;
 
         // Action
-	private int _time = 0;
+        private int _time = 0;
 
         #region IDemoScreen Members
 
@@ -61,8 +61,8 @@ namespace gearit.src.game
             _world.Gravity = new Vector2(0f, 9.8f);
             //clearRobot();
             SerializerHelper.World = _world;
-	    addRobot((Robot)Serializer.DeserializeItem("r2d2.gir"));
-        _robots[0].getPiece(Vector2.Zero).Weight = 30;
+            addRobot((Robot)Serializer.DeserializeItem("r2d2.gir"));
+            _robots[0].getPiece(Vector2.Zero).Weight = 30;
             _map = (Map)Serializer.DeserializeItem("moon.gim");
             // Loading may take a while... so prevent the game from "catching up" once we finished loading
             ScreenManager.Game.ResetElapsedTime();
@@ -94,7 +94,7 @@ namespace gearit.src.game
             if (_robots.Count == 1)
                 _camera.TrackingBody = robot.getHeart();
             robot.turnOn();
-	    robot.move(new Vector2(0, -20));
+            robot.move(new Vector2(0, -20));
         }
 
         public override void Update(GameTime gameTime, bool otherScreenHasFocus, bool coveredByOtherScreen)
@@ -116,7 +116,7 @@ namespace gearit.src.game
         {
             if (Input.pressed(Keys.Escape))
             {
-		clearRobot();
+                clearRobot();
                 ScreenManager.RemoveScreen(this);
             }
             if (Input.justPressed(MouseKeys.WHEEL_DOWN))
