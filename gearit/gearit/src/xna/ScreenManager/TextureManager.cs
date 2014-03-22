641964f9773a8369142e3f08850368ec5a501480
Arthur SFEZ <arthur.sfez@gmail.com>
2014-03-22 00:43:15 +0000

    Adding TextureManager

---
+++ gearit/gearit/src/xna/ScreenManager/TextureManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using System.Collections.Generic;
+
+namespace gearit.src.xna.ScreenManager
+{
+    class TextureManager
+    {
+        public enum ETexture
+        {
+            Diamond,
+            Spade,
+            Club,
+            Heart
+        };
+        private Dictionary<ETexture, Texture2D> _textures;
+        private static TextureManager _instance = null;
+
+        private TextureManager()
+        {
+            _textures = new Dictionary<ETexture, Texture2D>();
+            _textures.Add(ETexture.Diamond, SquidXNA.Game.ContentManager.Load<Texture2D>("diamond"));
+            _textures.Add(ETexture.Spade, SquidXNA.Game.ContentManager.Load<Texture2D>("spade"));
+            _textures.Add(ETexture.Club, SquidXNA.Game.ContentManager.Load<Texture2D>("club"));
+            _textures.Add(ETexture.Heart, SquidXNA.Game.ContentManager.Load<Texture2D>("heart"));
+        }
+
+        public static TextureManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new TextureManager();
+                return _instance;
+            }
+        }
+
+        public Texture2D getTexture(ETexture t)
+        {
+            return _textures[t];
+        }
+    }
+}
