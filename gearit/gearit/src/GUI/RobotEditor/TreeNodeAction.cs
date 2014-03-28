8df78909772641eeb02d4ce2bfb761f802bb5014
Shinao <rmonnerat.pro@gmail.com>
2014-03-28 17:07:00 +0800

    Selecting motor action

--- gearit/gearit/src/GUI/RobotEditor/TreeNodeAction.cs
+++ gearit/gearit/src/GUI/RobotEditor/TreeNodeAction.cs
@@ -43,8 +43,69 @@ namespace gearit.src.GUI.RobotEditor
                 btn_select.Text = "Select a piece";
                 _selecting = true;
                 _old_spot = _robot_editor._spot;
+                _robot_editor.selectHeart();
             };
             Content.Controls.Add(btn_select);
+
+            x += btn_select.Size.x + PADDING * 3;
+
+
+            lb = new Label();
+            lb.Text = "Motor";
+            lb.Size = new Point(50, height);
+            lb.Position = new Point(x, 0);
+            lb.Style = "treeNodeText";
+            Content.Controls.Add(lb);
+
+            x += lb.Size.x + PADDING;
+
+            // Event type choice
+            Content.Controls.Add(combo);
+            combo.Size = new Squid.Point(158, height / 2);
+            combo.Position = new Squid.Point(x, height / 2 - combo.Size.y / 2);
+            combo.Label.Style = "comboLabel";
+            combo.Button.Style = "comboButton";
+            combo.Listbox.Margin = new Margin(0, 0, 0, 0);
+            combo.Listbox.Style = "frame";
+            combo.Listbox.ClipFrame.Margin = new Margin(8, 8, 8, 8);
+            combo.Listbox.Scrollbar.Margin = new Margin(0, 4, 4, 4);
+            combo.Listbox.Scrollbar.Size = new Squid.Point(14, 10);
+            combo.Listbox.Scrollbar.ButtonUp.Style = "vscrollUp";
+            combo.Listbox.Scrollbar.ButtonUp.Size = new Squid.Point(10, 20);
+            combo.Listbox.Scrollbar.ButtonDown.Style = "vscrollUp";
+            combo.Listbox.Scrollbar.ButtonDown.Size = new Squid.Point(10, 20);
+            combo.Listbox.Scrollbar.Slider.Margin = new Margin(0, 2, 0, 2);
+            combo.Listbox.Scrollbar.Slider.Style = "vscrollTrack";
+            combo.Listbox.Scrollbar.Slider.Button.Style = "vscrollButton";
+
+            combo.Opened += delegate(Control sender, SquidEventArgs args)
+            {
+                combo.Dropdown.Position = new Point(combo.Dropdown.Position.x, combo.Dropdown.Position.y - combo.Dropdown.Size.y - combo.Size.y);
+            };
+
+            // Button pressed
+            ListBoxItem item = new ListBoxItem();
+            item.Text = "Forward";
+            item.Size = new Squid.Point(100, 35);
+            item.Margin = new Margin(0, 0, 0, 4);
+            item.Style = "item";
+            combo.Items.Add(item);
+
+            item = new ListBoxItem();
+            item.Text = "Backward";
+            item.Size = new Squid.Point(100, 35);
+            item.Margin = new Margin(0, 0, 0, 4);
+            item.Style = "item";
+            combo.Items.Add(item);
+
+            item = new ListBoxItem();
+            item.Text = "Stopped";
+            item.Size = new Squid.Point(100, 35);
+            item.Margin = new Margin(0, 0, 0, 4);
+            item.Style = "item";
+            combo.Items.Add(item);
+
+            item.Selected = true;
 
 
             // Button remove
