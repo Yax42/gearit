f453b6815ef78e370543cc88d59d11ec828aa64e
Jean Brunier <brunier42@gmail.com>
2014-06-21 10:02:07 +0200

    - Update default map/robot/script.

--- gearit/gearit/bin/x86/Debug/data/script/default.lua
+++ gearit/gearit/bin/x86/Debug/data/script/default.lua
@@ -2,15 +2,15 @@ require 'header'
 jump_time = 0
 
 while true do
-// #!#! Lua generated - Do not modify
-// Generated
-if Input:pressed(K_D) then
+-- #!#! Lua generated - Do not modify
+-- Generated
+if Input:pressed(K_A) then
 spot0.motor = 1
 spot1.motor = 1
 end
-if Input:pressed(K_A) then
+if Input:pressed(K_D) then
 spot1.motor = -1
 spot0.motor = -1
 end
-// #!#! End Lua generated
+-- #!#! End Lua generated
 end
