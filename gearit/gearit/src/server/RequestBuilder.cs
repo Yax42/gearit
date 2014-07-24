de4b7b2f13eceafcc95b3770c6ace38072942d52
Aliou Diallo <code@aliou.me>
2014-07-24 14:21:33 +0200

    Create RequestBuilder class.

---
+++ gearit/gearit/src/server/RequestBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Lidgren.Network;
+
+namespace gearit.src.server
+{
+
+    class RequestBuilder
+    {
+        public const int SAY_READY = 20;
+        public const int ROBOT_SELECT = 21;
+        public const int ROBOT_DL = 22;
+        public const int DLR_STATUS = 23;
+        public const int MAP_SELECT = 24;
+        public const int MAP_CHECK = 25;
+        public const int MAP_OK = 26;
+        public const int MAP_KO = 27;
+        public const int MAP_DL = 28;
+        public const int MAP_DL_OK = 29;
+        public const int MAP_DL_KO = 30;
+        // public const int START_GAME = 20;
+        // public const int END_GAME = 20;
+        public const int OK = 4;
+        public const int KO = 5;
+
+    }
+}
