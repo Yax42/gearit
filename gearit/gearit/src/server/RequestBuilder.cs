8fff1809a7c14060659b943a6cd135259ce9cc0a
Aliou Diallo <code@aliou.me>
2014-07-28 12:07:09 +0200

    Prefix ids constants.

--- gearit/gearit/src/server/RequestBuilder.cs
+++ gearit/gearit/src/server/RequestBuilder.cs
@@ -27,21 +27,21 @@ namespace gearit.src.server
 
     class RequestBuilder
     {
-        public const int SAY_READY = 20;
-        public const int ROBOT_SELECT = 21;
-        public const int ROBOT_DL = 22;
-        public const int DLR_STATUS = 23;
-        public const int MAP_SELECT = 24;
-        public const int MAP_CHECK = 25;
-        public const int MAP_OK = 26;
-        public const int MAP_KO = 27;
-        public const int MAP_DL = 28;
-        public const int MAP_DL_OK = 29;
-        public const int MAP_DL_KO = 30;
-        public const int START_GAME = 31;
-        public const int END_GAME = 32;
-        public const int OK = 4;
-        public const int KO = 5;
+        public const int REQ_SAY_READY = 20;
+        public const int REQ_ROBOT_SELECT = 21;
+        public const int REQ_ROBOT_DL = 22;
+        public const int REQ_DLR_STATUS = 23;
+        public const int REQ_MAP_SELECT = 24;
+        public const int REQ_MAP_CHECK = 25;
+        public const int REQ_MAP_OK = 26;
+        public const int REQ_MAP_KO = 27;
+        public const int REQ_MAP_DL = 28;
+        public const int REQ_MAP_DL_OK = 29;
+        public const int REQ_MAP_DL_KO = 30;
+        public const int REQ_START_GAME = 31;
+        public const int REQ_END_GAME = 32;
+        public const int REQ_OK = 4;
+        public const int REQ_KO = 5;
 
     }
 }
