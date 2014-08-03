de18a72d12963d89bb5104c03894d6701dc4a8da
unknown <rmonnerat.pro@gmail.com>
2014-08-03 19:42:37 +0200

    [Play] Identification master server - disconnect

--- gearit/gearit/src/GUI/MenuPlay.cs
+++ gearit/gearit/src/GUI/MenuPlay.cs
@@ -28,11 +28,19 @@ namespace gearit.src.gui
 
     class MenuPlay : GameScreen, IDemoScreen
 	{
+        TextBox tb_login;
+        TextBox tb_password;
+        Panel list_server;
         Task taskMasterServer = null;
         Desktop _desktop;
         bool refreshing = false;
         Button btn_refresh = new Button();
+        TcpClient client;
+        Label lb_error;
+        Panel dialog_co;
+        Button btn_co;
 
+        const int DIALOG_WIDTH = 400;
         const int TAB_WIDTH = 156;
         ListView olv = new ListView();
         List<MyData> models = new List<MyData>();
@@ -43,11 +51,11 @@ namespace gearit.src.gui
             public int Ping;
         }
 
-        public void th_getServers()
+        public void th_authenticate()
         {
             try
             {
-                TcpClient client = new TcpClient();
+                client = new TcpClient();
                 client.Connect("127.0.0.1", 7979);
 
                 // Authentification
@@ -55,8 +63,8 @@ namespace gearit.src.gui
                 byte[] req = new byte[packet_size];
 
                 req[0] = 4;
-                string login = "test";
-                string password = "test";
+                string login = tb_login.Text;
+                string password = tb_password.Text;
                 int i;
                 for (i = 0; i < login.Length; ++i)
                     req[i + 1] = Convert.ToByte(login[i]);
@@ -73,9 +81,33 @@ namespace gearit.src.gui
                 if (data[0] != 5)
                 {
                     ChatBox.addEntry("Wrong identification to master server", ChatBox.Entry.Info);
+                    lb_error.Text = "Login/Password incorrect";
+                    btn_co.Enabled = true;
+                    client.GetStream().Dispose();
                     return;
                 }
                 //
+                dialog_co.Parent = null;
+                list_server.Parent = _desktop;
+            }
+            catch (SocketException e)
+            {
+                ChatBox.addEntry("Could not connect to master server", ChatBox.Entry.Error);
+                lb_error.Text = "Could not connect to master server";
+            }
+
+            btn_co.Enabled = true;
+        }
+
+        public void th_getServers()
+        {
+            try
+            {
+                const int packet_size = 65;
+                byte[] req = new byte[packet_size];
+                int i;
+                byte[] data = new byte[1024];
+               
 
                 // Add dummy servers - TO REMOVE
                 Array.Clear(req, 0, req.Length);
@@ -90,6 +122,8 @@ namespace gearit.src.gui
                 Buffer.BlockCopy(ports, 0, req, 35, 4);
                 req[39] = 6;
                 client.GetStream().Write(req, 0, 40);
+                //
+
 
                 // Get the servers
                 req[0] = 1;
@@ -123,7 +157,7 @@ namespace gearit.src.gui
                     olv.SetObjects(models);
 
                     // Shutdown
-                    client.GetStream().Dispose();
+                    //client.GetStream().Dispose();
                 }
 
             }
@@ -139,15 +173,92 @@ namespace gearit.src.gui
 		public override void LoadContent()
 		{
 			base.LoadContent();
+
+            VisibleMenu = true;
+
             _desktop = new Desktop();
             _desktop.Position = new Squid.Point(MainMenu.MENU_WIDTH, 0);
             _desktop.Size = new Squid.Point(ScreenManager.Width - MainMenu.MENU_WIDTH, ScreenManager.Height);
 
+            dialog_co = new Panel();
+            dialog_co.Position = new Squid.Point(ScreenManager.Width / 2 - DIALOG_WIDTH, ScreenManager.Height / 4);
+            dialog_co.Size = new Squid.Point((ScreenManager.Width - MainMenu.MENU_WIDTH) / 2, DIALOG_WIDTH / 2 + 24);
+            dialog_co.Parent = _desktop;
+            dialog_co.Style = "menu";
 
+            Label TitleLabel = new Label();
+            TitleLabel.Size = new Squid.Point(100, 45);
+            TitleLabel.Dock = DockStyle.Top;
+            TitleLabel.Text = "CONNEXION";
+            TitleLabel.Style = "itemMenuTitle";
+            TitleLabel.TextAlign = Alignment.MiddleLeft;
+            TitleLabel.Margin = new Margin(0, 0, 0, -1);
+            dialog_co.Content.Controls.Add(TitleLabel);
+
+            Frame ButtonFrame = new Frame();
+            ButtonFrame.Size = new Squid.Point(100, 35);
+            ButtonFrame.Dock = DockStyle.Bottom;
+            dialog_co.Content.Controls.Add(ButtonFrame);
+
+            tb_login = new TextBox();
+            tb_login.Text = "test";
+            tb_login.Size = new Squid.Point(158, 34);
+            tb_login.Position = new Squid.Point(dialog_co.Size.x / 2 - 16, 70);
+            dialog_co.Content.Controls.Add(tb_login);
+            tb_login.Focus();
+
+            Label lb = new Label();
+            lb.Position = new Squid.Point(0, 70);
+            lb.Size = new Squid.Point((int)((float)dialog_co.Size.x / 2.5), 34);
+            lb.TextAlign = Alignment.MiddleRight;
+            lb.Text = "Login";
+            dialog_co.Content.Controls.Add(lb);
+
+            lb = new Label();
+            lb.Position = new Squid.Point(0, 120);
+            lb.Size = new Squid.Point((int) ((float) dialog_co.Size.x / 2.5), 34);
+            lb.TextAlign = Alignment.MiddleRight;
+            lb.Text = "Password";
+            dialog_co.Content.Controls.Add(lb);
+
+            tb_password = new TextBox();
+            tb_password.Text = "test";
+            tb_password.Size = new Squid.Point(158, 34);
+            tb_password.Position = new Squid.Point(dialog_co.Size.x / 2 - 16, 120);
+            dialog_co.Content.Controls.Add(tb_password);
+
+            btn_co = new Button();
+            btn_co.Size = new Squid.Point(124, 34);
+            btn_co.Position = new Squid.Point(dialog_co.Size.x - 124 - 8, dialog_co.Size.y - 34 - 8);
+            btn_co.Text = "Connexion";
+            dialog_co.Content.Controls.Add(btn_co);
+
+            lb_error = new Label();
+            lb_error.Size = new Squid.Point(dialog_co.Size.x / 2, 34);
+            lb_error.Position = new Squid.Point(0, dialog_co.Size.y - 34 - 8);
+            lb_error.TextAlign = Alignment.MiddleCenter;
+            lb_error.TextColor = ColorInt.RGBA(.8f, .0f, .0f, 1);
+            dialog_co.Content.Controls.Add(lb_error);
+
+            btn_co.MouseClick += delegate(Control snd, MouseEventArgs e)
+            {
+                btn_co.Enabled = false;
+                taskMasterServer = new Task(() =>
+                {
+                    th_authenticate();
+                });
+                taskMasterServer.Start();
+            };
+
+
+            list_server = new Panel();
+            list_server.Position = new Squid.Point(0, 0);
+            list_server.Parent = null;
+            list_server.Dock = DockStyle.Fill;
             TabControl tabcontrol = new TabControl();
             tabcontrol.ButtonFrame.Style = "item";
             tabcontrol.Dock = DockStyle.Fill;
-            tabcontrol.Parent = _desktop;
+            list_server.Content.Controls.Add(tabcontrol);
 
 
             TabPage tabInternet = new TabPage();
@@ -198,14 +309,24 @@ namespace gearit.src.gui
 
             tabcontrol.SelectedTab = tabInternet;
 
-            btn_refresh.Parent = _desktop;
             btn_refresh.Size = new Squid.Point(100, 26);
             btn_refresh.Position = new Squid.Point(TAB_WIDTH * 2 + 24, 4);
             btn_refresh.Text = "Refresh";
             btn_refresh.Style = "addEventButton";
             btn_refresh.MouseClick += new MouseEvent(btn_MouseClick);
+            list_server.Content.Controls.Add(btn_refresh);
 
-            VisibleMenu = true;
+            Button btn_disconnect = new Button();
+            btn_disconnect.Size = new Squid.Point(100, 26);
+            btn_disconnect.Position = new Squid.Point(TAB_WIDTH * 2 + 24 + 126, 4);
+            btn_disconnect.Text = "Disconnect";
+            btn_disconnect.MouseClick += delegate(Control snd, MouseEventArgs e)
+            {
+                client.GetStream().Dispose();
+                list_server.Parent = null;
+                dialog_co.Parent = _desktop;
+            };
+            list_server.Content.Controls.Add(btn_disconnect);
 		}
 
         void btn_MouseClick(Control sender, MouseEventArgs args)
