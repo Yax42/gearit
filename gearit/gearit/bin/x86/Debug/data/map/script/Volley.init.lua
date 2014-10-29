45ea9789694e5e5b4438043c9db34a5d25dd0fd5
Raphael Monnerat <rmonnerat.pro@gmail.com>
2014-10-29 19:08:30 +0100

    Volley mode

---
+++ gearit/gearit/bin/x86/Debug/data/map/script/Volley.init.lua
@@ -0,0 +1,8 @@
+nb_player = 0
+
+Object_Ball.Static = true
+Object_Ball.Mass = 10
+Object_Ball.Position = Art_0.Position
+left_turn = true
+right_turn = false
+team = true
---
+++ gearit/gearit/bin/x86/Debug/data/map/script/Volley.robot.init.lua
@@ -0,0 +1,6 @@
+if nb_player == 0 then
+	Robot:MoveTo(Art_2)
+elseif nb_player == 1 then
+	Robot:MoveTo(Art_3)
+end
+nb_player = nb_player + 1
